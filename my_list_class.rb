2457d7ae3579613067d42dd8cd36c390ae4bd11b
Omar Farouq <ofarouq310@gmail.com>
2023-06-22 13:19:42 +0300

    Update my_list_class.rb

--- my_list_class.rb
+++ my_list_class.rb
@@ -9,3 +9,7 @@ class MyList
     @list.each(&block)
   end
 end
+my_list = MyList.new([1,2,3,4])
+puts my_list.all? {|element| element > 0}
+puts my_list.any? {|element| element < 0}
+p my_list.filter? {|element| element.even? }
